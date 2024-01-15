6cb738cc8352dc9348ce84a965a0baa8104640d0
Harsh Bhardwaj <110498182+HarshBhardwaj08@users.noreply.github.com>
2024-01-16 01:37:41 +0530

    Lvl Design

--- Assets/GemsUIManager.cs
+++ Assets/GemsUIManager.cs
@@ -8,10 +8,7 @@ using UnityEngine.UI;
 public class GemsUIManager : MonoBehaviour
 {
     public Text Scoretext;
-    private void Awake()
-    {
-        
-    }
+  
     private void OnEnable()
     {
         SignalManager.Instance.Subscribe<UpdateGemsUISignal>(UpdateGemsUI);
@@ -20,5 +17,9 @@ public class GemsUIManager : MonoBehaviour
     private void UpdateGemsUI(UpdateGemsUISignal signal)
     {
         Scoretext.text = "Gems : " + signal.gemScore.ToString();
+    }
+    private void OnDisable()
+    {
+        SignalManager.Instance.Unsubscribe<UpdateGemsUISignal>(UpdateGemsUI);
     }
 }
